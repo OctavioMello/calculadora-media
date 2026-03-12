a870ecc7790f93766356c425daf105ff4a8d248d
Octávio Mello Covre de Souza <octaviomellocovredesouza@gmail.com>
2026-03-12 19:31:54 +0000

    adicionando maior número, menor número e quantidade de números

--- script.js
+++ script.js
@@ -16,11 +16,20 @@ console.log(lista)
 function media(lista) {
     let soma = 0;
     let media;
+    let maior = lista[0];
+    let menor = lista[0];
     for (let i = 0; i < lista.length; i++) {
         soma += lista[i];
+        if (lista[i] > maior) {
+            maior = lista[i];
+        }
+        if (lista[i] < menor) {
+            menor = lista[i];
+        }
         } media = soma / lista.length
-        return Number(media.toFixed(1))
+        return [Number(media.toFixed(1)), maior, menor, lista.length];
 }
 
 let resultado = media(lista);
-alert(`A média das suas notas é ${resultado}`);
+console.log(resultado);
+alert(`Você informou ${resultado[3]} notas, sendo a média delas igual a ${resultado[0]}, a maior delas ${resultado[1]} e a menor delas ${resultado[2]}`);
